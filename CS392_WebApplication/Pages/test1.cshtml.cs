4b77c179c6f9ea51250c547f20b4727aa7644c7c
White, Jordan <jwhite49@jacksonville.edu>
2026-01-15 16:34:01 -0500

    adding a test page to the cart feature

---
+++ CS392_WebApplication/Pages/test1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS392_WebApplication.Pages
+{
+    public class test1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
