4f07837f11927c7326a36d2e187c441b6d2c5deb
White, Jordan <jwhite49@jacksonville.edu>
2026-02-03 16:43:18 -0500

    Implementing roles and database v1.0

---
+++ CS392_WebApplication/Data/DbSeeder.cs
@@ -0,0 +1,33 @@
+﻿using CS392_WebApplication.Constants;
+using Microsoft.AspNetCore.Identity;
+using System.Data;
+
+namespace CS392_WebApplication.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider service)
+        {
+            //Seed Roles
+            var userManager = service.GetService<UserManager<IdentityUser>>();
+            var roleManager = service.GetService<RoleManager<IdentityRole>>();
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            //creating admin
+            var user = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            var userInDb = await userManager.FindByEmailAsync(user.Email);
+            if (userInDb == null)
+            {
+                await userManager.CreateAsync(user, "Admin@123");
+                await userManager.AddToRoleAsync(user, Roles.Admin.ToString());
+            }
+        }
+    }
+}
--- CS392_WebApplication/Program.cs
+++ CS392_WebApplication/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +16,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+/* builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllersWithViews();
+*/
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
