836313ae92ea2693560bb567b00f5b16b5cd5f64
JAYxSAUSE <jaydenlop@icloud.com>
2026-02-12 21:36:58 -0500

    Added dtabase tables

---
+++ CS392_WebApplication/Models/Product_list.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS392_WebApplication.Models
+{
+    public class Product_list
+    {
+        [Key]public int list_ID { get; set; }
+        [ForeignKey("User")] public int user_ID { get; set; }
+        [Required] public float total_price { get; set; }
+        [Required] public string list_type { get; set; }
+        //whether list type is for user / school 
+        //could possibly chage it to bool, false for schoool , true for user 
+        public string amazon_wishlist_url { get; set; }
+        [Required] public DateTime created_at { get; set; }
+    }
+}
---
+++ CS392_WebApplication/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS392_WebApplication.Models
+{
+    public class User
+    {
+        [Key] public int user_id { get; set; }
+
+        public string username { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string password_hash {get; set;}
+
+    }
+}
