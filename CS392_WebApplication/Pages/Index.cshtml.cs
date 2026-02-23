9a06430a065496662d18e796086d0b33599aa633
jl <jaydenlop@icloud.com>
2026-02-22 20:58:24 -0500

    Implemented login popup with transparent BG, still need to apply transitioning and positioning css

--- CS392_WebApplication/Pages/Index.cshtml.cs
+++ CS392_WebApplication/Pages/Index.cshtml.cs
@@ -5,6 +5,12 @@ namespace CS392_WebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        public string Username { get; set; } = string.Empty;
+        [BindProperty]
+        public string Password { get; set; } = string.Empty;
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
--- CS392_WebApplication/wwwroot/js/site.js
+++ CS392_WebApplication/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function openModal(){
+    document.getElementById("loginModal").style.display = "block";
+}
+function closeModal(){
+    document.getElementById("loginModal").style.display = "none";
+    /* if user login successful then screen closes out */
+}
