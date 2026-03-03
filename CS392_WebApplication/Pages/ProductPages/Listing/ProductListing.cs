88156d17061833d28bf3c1ded3333dadfd0950c2
JAYxSAUSE <jaydenlop@icloud.com>
2026-03-02 22:56:37 -0500

    * table spacing for easier viewing

--- CS392_WebApplication/Pages/ProductPages/Listing/ProductListing.cs
+++ CS392_WebApplication/Pages/ProductPages/Listing/ProductListing.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security;
 
 namespace CS392_WebApplication.Pages.ProductPages.Listing
 {
@@ -19,13 +20,28 @@ namespace CS392_WebApplication.Pages.ProductPages.Listing
 
         public IList<Products> Products { get; set; } = default!;
         public Products Product { get; set; } = default!; //single product instead of a list
-        public async Task OnGetAsync(string category)//takes category whichb is passed in the query string and filters products based on that category
+       
+        public async Task<IActionResult> OnGetAsync(string category)//takes category whichb is passed in the query string and filters products based on that category
         {
-            Products = await _context.Products
-                .Where(p => p.product_name.Contains(category/* StringComparison.OrdinalIgnoreCase*/))
-                .ToListAsync();
-            //ordinalIgnoreCase does not check for case senssitivity
-            Product = Products.FirstOrDefault(); //get the first product from the list of products
+
+            //checks if there is data to be passed to search for product
+            //Needs to be revisited
+            if (String.IsNullOrWhiteSpace(category))
+            {
+                _logger.LogWarning("Error finding category: {category}", category);
+                return NotFound();
+            }
+            else
+            {
+               Products = await _context.Products
+                    .Where(p => p.product_name.Contains(category/* StringComparison.OrdinalIgnoreCase*/))
+                    .ToListAsync();
+                    //ordinalIgnoreCase does not check for case senssitivity
+               Product = Products.FirstOrDefault(); //get the first product from the list of products
+               return Page();
+            } 
+     
+
 
         }
     }
